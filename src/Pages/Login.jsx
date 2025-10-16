cad2f513946d8e45a6be620dfc64a2c81b446768
Ariful Islam <arifulq234@gmail.com>
2025-10-16 21:01:44 +0600

    Register and login  form adde

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
     return (
-        <div>
-                <h1>This is login</h1>
-        </div>
+       <div className="card bg-base-100 w-full max-w-sm mx-auto my-10 shrink-0 shadow-2xl">
+           <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Please Login!</h1>
+      
+    </div>
+      <div className="card-body">
+        <form>
+            <fieldset className="fieldset">
+           Please Login
+            {/* Email Section */}
+          <label className="label">Email</label>
+          <input type="email" name='email' className="input" placeholder="Email" />
+          {/* Password Section */}
+          <label className="label">Password</label>
+          <input type="password" name='password' className="input" placeholder="Password" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+         <h6>New to website? please <Link className='text-blue-500 hover:underline  group-hover:vhover:text-blue-800' to={'/register'}>Register</Link> </h6>
+        </form>
+      </div>
+    </div>
+
     );
 };
 
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
     return (
-        <div>
-                <h1>This is register Section</h1>
-        </div>
+
+ 
+    <div className="card bg-base-100 w-full max-w-sm mx-auto my-10 shrink-0 shadow-2xl">
+           <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Register Now!</h1>
+      
+    </div>
+      <div className="card-body">
+        <form>
+            <fieldset className="fieldset">
+            {/* Name Section */}
+          <label className="label">Name</label>
+          <input type="text" name='name' className="input" placeholder="Your Name" />
+            {/* Email Section */}
+          <label className="label">Email</label>
+          <input type="email" name='email' className="input" placeholder="Email" />
+          {/* Password Section */}
+          <label className="label">Password</label>
+          <input type="password" name='password' className="input" placeholder="Password" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Register</button>
+        </fieldset>
+         <h6>Alreay Have an account? please <Link className='text-blue-500 hover:underline  group-hover:vhover:text-blue-800' to={'/login'}>Login</Link> </h6>
+        </form>
+      </div>
+    </div>
+
     );
 };
 
